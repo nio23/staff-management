b682c864329b250cf2d6ea27437fa8129c4fb83c
Home <nio_dapro@hotmail.com>
2023-05-11 23:05:29 +0300

    Initialize

---
+++ IndeavorChallenge/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace IndeavorChallenge.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string surname { get; set; }
+        public string name { get; set; }
+        public List<Skill> skills { get; set; }
+        public DateTime hiringDate { get; set; }
+
+    }
+}
---
+++ IndeavorChallenge/Models/Skill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IndeavorChallenge.Models
+{
+    public class Skill
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime dateCreated { get; set; }
+    
+
+    }
+}
